2a1379c31028e501619959ebc84b05af68590112
Tuki <pachadomenic2010@gmail.com>
2026-07-19 14:39:27 +0300

    Revert "AlphaDroid Bringup"
    This reverts commit 0ac907ba13c254349a75d5c9d6c5df03f2bf6725.

---
+++ lineage_dodge.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021-2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from dodge device
+$(call inherit-product, device/oneplus/dodge/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+PRODUCT_NAME := lineage_dodge
+PRODUCT_DEVICE := dodge
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2653
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1778292471529 release-keys" \
+    BuildFingerprint=OnePlus/CPH2653EEA/OP5D55L1:16/BP2A.250605.015/V.R4T3.1acb2c0_8f96c8_8d5fa9:user/release-keys \
+    DeviceName=OP5D55L1 \
+    DeviceProduct=CPH2653 \
+    SystemDevice=OP5D55L1 \
+    SystemName=CPH2653
